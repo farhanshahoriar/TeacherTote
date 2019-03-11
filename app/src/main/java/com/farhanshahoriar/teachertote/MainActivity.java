ba8ef1b61ff4154f73b6eeef1fb534d403af0270
mahade10 <mahadeabirabir@gmail.com>
2019-03-11 11:24:41 +0600

    main activity class added with class selection options

--- app/src/main/java/com/farhanshahoriar/teachertote/MainActivity.java
+++ app/src/main/java/com/farhanshahoriar/teachertote/MainActivity.java
@@ -1,7 +1,14 @@
 package com.farhanshahoriar.teachertote;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.VisibleForTesting;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //TextView view =(TextView) findViewById(R.id.welc_tv);
+        //getApplicationContext().getDataDir().toString()
+
+        //view.setText( getApplicationContext().getFilesDir().toString());
+    }
+
+    public void onClickClass1Result(View view){
+        Toast.makeText(this,"BtnOne",Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this,DisplayResult.class);
+        intent.putExtra("Class","1");
+
+        startActivity(intent);
+
+    }
+    public void onClkClass2Result(View view){
+
+        //Toast.makeText(this,"BtnOne",Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this,DisplayResult.class);
+        intent.putExtra("Class","2");
+        startActivity(intent);
+
+    }
+    public void onClkClass3Result(View view){
+
+        //Toast.makeText(this,"BtnOne",Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this,DisplayResult.class);
+        intent.putExtra("Class","3");
+        startActivity(intent);
+
+    }
+    public void onClkClass4Result(View view){
+
+        //Toast.makeText(this,"BtnOne",Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this,DisplayResult.class);
+        intent.putExtra("Class","4");
+        startActivity(intent);
+
+    }
+    public void onClkClass5Result(View view){
+
+        //Toast.makeText(this,"BtnOne",Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this,DisplayResult.class);
+        intent.putExtra("Class","5");
+        startActivity(intent);
+
     }
 }
