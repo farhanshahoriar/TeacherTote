ab58dec1329c5197b6435c06f34a9257b2cb29d5
mahade10 <mahadeabirabir@gmail.com>
2019-03-11 13:07:09 +0600

    Extra lines deleted

--- app/src/main/java/com/farhanshahoriar/teachertote/MainActivity.java
+++ app/src/main/java/com/farhanshahoriar/teachertote/MainActivity.java
@@ -16,10 +16,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //TextView view =(TextView) findViewById(R.id.welc_tv);
-        //getApplicationContext().getDataDir().toString()
-
-        //view.setText( getApplicationContext().getFilesDir().toString());
     }
 
     public void onClickClass1Result(View view){
